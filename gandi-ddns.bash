fc670ae97017b3b470b7a78dd35be65e70cde1bd
anagno <anagnwstopoulos@hotmail.com>
2019-07-22 21:16:42 +0200

    Adding the .travis.yml and adapting the docker secrets

--- gandi-ddns.bash
+++ gandi-ddns.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -o pipefail
 # 
 # Original script taken from: 
 # https://github.com/Gandi/api-examples/blob/master/bash/livedns/mywanip.sh
@@ -9,15 +10,44 @@
 # Updates a zone record using Gandi's LiveDNS.
 
 
+# https://github.com/docker-library/postgres/blob/cf9b6cdd64f8a81b1abf9e487886f47e4971abe2/11/docker-entrypoint.sh
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env 'DOMAIN'
+
 if [[ -z "$DOMAIN" ]]; then
     echo "[ERROR] Mandatory variable DOMAIN not defined."
     exit 1
 fi
 
+file_env 'SUBDOMAIN'
+
 if [[ -z "$SUBDOMAIN" ]]; then
     echo "[ERROR] Mandatory variable SUBDOMAIN not defined."
     exit 1
 fi
+
+file_env 'APIKEY'
 
 if [[ -z "$APIKEY" ]]; then
     echo "[ERROR] Mandatory variable APIKEY not defined."
